89caf0f81fe2cd63769c50b02266d4c98ebb4cc3
Alex Foster <xrk4np@virginia.edu>
2024-01-18 00:22:45 -0500

    get discord auth working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,38 @@
-import Link from "next/link";
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
 
-import { getServerAuthSession } from "@/server/auth";
-import { api } from "@/trpc/server";
+function Home() {
+  const session = useSession();
+  return (
+    <main className="grid h-full place-content-center bg-slate-50">
+      <button
+        type="button"
+        className="rounded-md border-2 border-slate-600 p-3"
+        onClick={() => {
+          signIn("discord");
+        }}
+      >
+        Sign In
+      </button>
+      <button
+        type="button"
+        disabled={session.status !== "authenticated"}
+        className="rounded-md border-2 border-slate-600 p-3 disabled:bg-red-200"
+        onClick={() => {
+          signOut();
+        }}
+      >
+        Sign Out
+      </button>
+    </main>
+  );
+}
 
-export default async function Home() {
-  return <main className="grid place-content-center bg-slate-50">Hi</main>;
+export default function App() {
+  return (
+    <SessionProvider>
+      <Home />
+    </SessionProvider>
+  );
 }
