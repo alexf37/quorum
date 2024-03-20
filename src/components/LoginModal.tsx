35ed6210e93ab073acbdceeac6967448e73eb26d
Alex Foster <xrk4np@virginia.edu>
2024-03-19 23:22:53 -0400

    get credential auth working

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -18,6 +18,8 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "./ui/tooltip";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export function LoginModal({
   children,
@@ -25,10 +27,18 @@ export function LoginModal({
 }: PropsWithChildren<{
   defaultOpen?: boolean;
 }>) {
+  const router = useRouter();
+
   const [open, setOpen] = useState(false);
   useEffect(() => {
     setOpen(!!defaultOpen);
   }, [defaultOpen]);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -40,46 +50,64 @@ export function LoginModal({
           </DialogDescription>
         </DialogHeader>
 
-        <TooltipProvider>
-          <Tooltip delayDuration={200}>
-            <TooltipTrigger asChild>
-              <div className="flex flex-col gap-4">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    disabled
-                    id="email"
-                    type="email"
-                    placeholder="mst3k@example.com"
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <Label htmlFor="password">Password</Label>
-                  <Input disabled id="password" type="password" />
-                </div>
-                <Button disabled className="w-full">
-                  Sign in
-                </Button>
-                <div className="relative">
-                  <div className="absolute inset-0 flex items-center">
-                    <span className="w-full border-t" />
-                  </div>
-                  <div className="relative flex justify-center text-xs uppercase">
-                    <span className="bg-background px-2 text-muted-foreground">
-                      Or continue with
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>
-                Email & password accounts are currently disabled. Choose a
-                provider below instead.
-              </p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            setLoading(true);
+            const res = await signIn("credentials", {
+              email,
+              password,
+              redirect: false,
+              callbackUrl: "/dashboard",
+            });
+            if (!res) {
+              setError("An error occurred");
+              setLoading(false);
+            } else if (!res.error) {
+              router.refresh();
+              location.href = "/dashboard";
+            } else {
+              setError(res.error);
+              setLoading(false);
+            }
+          }}
+          className="flex flex-col gap-4"
+        >
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              id="email"
+              type="email"
+              placeholder="mst3k@example.com"
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              id="password"
+              type="password"
+            />
+          </div>
+          <div className="text-sm text-destructive">{error}</div>
+          <Button disabled={loading} className="w-full">
+            Sign in
+          </Button>
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">
+                Or continue with
+              </span>
+            </div>
+          </div>
+        </form>
+
         <div className="flex flex-col gap-3">
           <OauthButtons />
         </div>
