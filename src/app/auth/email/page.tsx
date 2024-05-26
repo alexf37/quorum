4d15d5dffd3ff11f7ecf585b539dcda594407073
Alex Foster <xrk4np@virginia.edu>
2024-05-26 00:23:01 -0400

    clean up email verification page

--- src/app/auth/email/page.tsx
+++ src/app/auth/email/page.tsx
@@ -2,28 +2,12 @@
 
 import { Button } from "@/components/ui/button";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-
-function useDelayedRender() {
-  const [render, setRender] = useState(false);
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setRender(true);
-    }, 1000);
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, []);
-  return render;
-}
 
 export default function EmailVerificationPage() {
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
   const email = searchParams.get("email");
   const callbackUrl = searchParams.get("callbackUrl");
-
-  //   const shouldRender = useDelayedRender();
 
   if (!token || !email || !callbackUrl) {
     return <p>Invalid URL</p>;
@@ -33,19 +17,24 @@ export default function EmailVerificationPage() {
     return <p>No browser detected.</p>;
   }
 
-  //   if (!shouldRender) {
-  //     return <p>Checking if you're human...</p>;
-  //   }
   return (
     <div className="grid h-full place-content-center">
+      <div className="space-y-2 text-center">
+        <h1 className="text-3xl font-bold">One more step...</h1>
+        <p className="max-w-sm text-muted-foreground">
+          Click the button below to login. <br /> You will stay logged in for 30
+          days.
+        </p>
+      </div>
       <form
+        className="mx-auto mt-4"
         method="GET"
         action={`/api/auth/callback/email?token=${token}&email=${email}&callbackUrl=${callbackUrl}`}
       >
         <input type="hidden" name="token" value={token} />
         <input type="hidden" name="callbackUrl" value={callbackUrl} />
         <input type="hidden" name="email" value={email} />
-        <Button>Click to Login</Button>
+        <Button>Login to Quorum</Button>
       </form>
     </div>
   );
