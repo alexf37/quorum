9ebb55a4ffcf421b9d07423921a4bd9dfeb8cc38
Alex Foster <xrk4np@virginia.edu>
2024-05-25 23:50:34 -0400

    delay render of button

--- src/app/auth/email/page.tsx
+++ src/app/auth/email/page.tsx
@@ -2,6 +2,20 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+function useDelayedRender() {
+  const [render, setRender] = useState(false);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setRender(true);
+    }, 1000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+  return render;
+}
 
 export default function EmailVerificationPage() {
   const router = useRouter();
@@ -10,8 +24,14 @@ export default function EmailVerificationPage() {
   const email = searchParams.get("email");
   const callbackUrl = searchParams.get("callbackUrl");
 
+  const shouldRender = useDelayedRender();
+
   if (!token || !email || !callbackUrl) {
     return <p>Invalid URL</p>;
+  }
+
+  if (!shouldRender) {
+    return <p>Checking if you're human...</p>;
   }
   return (
     <div className="grid h-full place-content-center">
